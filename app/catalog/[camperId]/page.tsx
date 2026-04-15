addc813e14d83d239352f74981caad07ed20c0e4
Olena <marineelen91@gmail.com>
2026-04-15 20:12:31 +0300

    create project, add catalog, add pages,add components

---
+++ app/catalog/[camperId]/page.tsx
@@ -0,0 +1,12 @@
+// import styles from "./page.module.css";
+
+const CamperDetailsPage = () => {   
+  return (
+    <div>
+      <h1>Camper Details</h1>
+      <p>Welcome to the camper details page!</p>
+    </div>
+  );
+}       
+
+export default CamperDetailsPage;
---
+++ app/catalog/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.css";
+
+const CatalogPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Catalog</h1>
+      <p>Welcome to the catalog page!</p>
+    </div>
+  );
+};
+
+export default CatalogPage;
